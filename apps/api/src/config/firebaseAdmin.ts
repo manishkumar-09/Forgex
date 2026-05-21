befb3cc6740da1aff3ae946cfc74106ba616f77a
Manish <iammanish0312@gmail.com>
2026-05-21 10:23:44 +0530

    feat: add Firebase admin

---
+++ apps/api/src/config/firebaseAdmin.ts
@@ -0,0 +1,14 @@
+import admin from "firebase-admin";
+import { ENV } from "./env";
+
+if (!admin.apps.length) {
+  admin.initializeApp({
+    credential: admin.credential.cert({
+      projectId: ENV.FIREBASE_PROJECT_ID,
+      clientEmail: ENV.FIREBASE_CLIENT_EMAIL,
+      privateKey: ENV.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
+    }),
+  });
+}
+
+export const authAdmin = admin.auth();
