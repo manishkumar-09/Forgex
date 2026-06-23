ba62f28fb3353cbee76433d1ce9dc27c355b99e6
Manish <iammanish0312@gmail.com>
2026-06-23 21:32:36 +0530

    health route added

---
+++ apps/api/src/routes/health.routes.ts
@@ -0,0 +1,7 @@
+import { Router } from "express";
+import healthController from "../controllers/health.controller";
+const healthRouter = Router();
+
+healthRouter.get("/check", healthController);
+
+export default healthRouter;
